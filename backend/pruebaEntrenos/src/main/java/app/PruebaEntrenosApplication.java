02df1dbfd76354753897b8d9875c5ec5ef2b4c07
Yisusandres <yisutronick235@gmail.com>
2026-05-21 19:59:59 -0400

    Estructura del backend - v2

--- backend/pruebaEntrenos/src/main/java/app/PruebaEntrenosApplication.java
+++ backend/pruebaEntrenos/src/main/java/app/PruebaEntrenosApplication.java
@@ -1,4 +1,4 @@
-package com.backend.pruebaEntrenos;
+package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/pruebaEntrenos/src/test/java/com/testEntreNos/PruebaEntrenosApplicationTests.java
+++ backend/pruebaEntrenos/src/test/java/com/testEntreNos/PruebaEntrenosApplicationTests.java
@@ -1,4 +1,4 @@
-package com.backend.pruebaEntrenos;
+package com.testEntreNos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
