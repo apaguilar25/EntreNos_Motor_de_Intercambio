d70479bb48d30759f566f9af76aa91701756cdbe
maveliz25-byte <maveliz.25@est.ucab.edu.ve>
2026-05-28 18:10:11 -0400

    Update IRepositorioPublicacion.java
    implementacion RepoPublicacion

--- backend/src/main/java/es/ucab/entrenos/modulos/publicacion/repositorios/IRepositorioPublicacion.java
+++ backend/src/main/java/es/ucab/entrenos/modulos/publicacion/repositorios/IRepositorioPublicacion.java
@@ -1,4 +1,10 @@
 package es.ucab.entrenos.modulos.publicacion.repositorios;
-
+import es.ucab.entrenos.modulos.publicacion.modelos.Publicacion;
+import java.util.List;
+import java.util.Optional;
 public interface IRepositorioPublicacion {
+    List<Publicacion> obtenerTodas();
+    Optional<Publicacion> obtenerPorId(String idPublicacion);
+    void guardar(Publicacion publicacion);
+    void guardarTodas(List<Publicacion> publicaciones);
 }
