7fbd9483f415a5b4b604f99b351f13362fa1c881
maveliz25-byte <maveliz.25@est.ucab.edu.ve>
2026-06-12 16:42:33 -0400

    Create ControladorNotificacion.java

---
+++ backend/src/main/java/es/ucab/entrenos/modulos/notificacion/controladores/ControladorNotificacion.java
@@ -0,0 +1,46 @@
+package es.ucab.entrenos.modulos.notificacion.controladores;
+
+import es.ucab.entrenos.modulos.notificacion.modelos.Notificacion;
+import es.ucab.entrenos.modulos.notificacion.servicios.ServicioNotificacion;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/notificaciones")
+@CrossOrigin(origins = "http://localhost:5173")
+public class ControladorNotificacion {
+    private final ServicioNotificacion servicioNotificacion;
+
+    public ControladorNotificacion(ServicioNotificacion servicioNotificacion) {
+        this.servicioNotificacion = servicioNotificacion;
+    }
+
+    @GetMapping("/{idDestinatario}")
+    public ResponseEntity<List<Notificacion>> obtenerNotificaciones(@PathVariable String idDestinatario) {
+        return ResponseEntity.ok(servicioNotificacion.obtenerNotificaciones(idDestinatario));
+    }
+
+    @GetMapping("/{idDestinatario}/no-leidas")
+    public ResponseEntity<List<Notificacion>> obtenerNoLeidas(@PathVariable String idDestinatario) {
+        return ResponseEntity.ok(servicioNotificacion.obtenerNoLeidas(idDestinatario));
+    }
+
+    @GetMapping("/{idDestinatario}/contar")
+    public ResponseEntity<Map<String, Integer>> contarNoLeidas(@PathVariable String idDestinatario) {
+        return ResponseEntity.ok(Map.of("count", servicioNotificacion.contarNoLeidas(idDestinatario)));
+    }
+
+    @PutMapping("/{idNotificacion}/leer")
+    public ResponseEntity<Void> marcarComoLeida(@PathVariable String idNotificacion) {
+        servicioNotificacion.marcarComoLeida(idNotificacion);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{idDestinatario}/leer-todas")
+    public ResponseEntity<Void> marcarTodasComoLeidas(@PathVariable String idDestinatario) {
+        servicioNotificacion.marcarTodasComoLeidas(idDestinatario);
+        return ResponseEntity.ok().build();
+    }
+}
