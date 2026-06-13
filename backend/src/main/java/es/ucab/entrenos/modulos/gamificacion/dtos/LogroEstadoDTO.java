127ca47fcf2c6e115279740981a4a3ad88104ace
maveliz25-byte <maveliz.25@est.ucab.edu.ve>
2026-06-13 10:17:15 -0400

    Create LogroEstadoDTO.java

---
+++ backend/src/main/java/es/ucab/entrenos/modulos/gamificacion/dtos/LogroEstadoDTO.java
@@ -0,0 +1,34 @@
+package es.ucab.entrenos.modulos.gamificacion.dtos;
+
+import es.ucab.entrenos.modulos.gamificacion.modelos.Logro;
+
+public class LogroEstadoDTO {
+
+    private String idLogro;
+    private String nombre;
+    private String descripcion;
+    private int bonoCreditos;
+    private String icono;
+    private boolean desbloqueado;
+    private Long fechaDesbloqueo;
+
+    public LogroEstadoDTO() {}
+
+    public LogroEstadoDTO(Logro logro, boolean desbloqueado, Long fechaDesbloqueo) {
+        this.idLogro = logro.getIdLogro();
+        this.nombre = logro.getNombre();
+        this.descripcion = logro.getDescripcion();
+        this.bonoCreditos = logro.getBonoCreditos();
+        this.icono = logro.getIcono();
+        this.desbloqueado = desbloqueado;
+        this.fechaDesbloqueo = fechaDesbloqueo;
+    }
+
+    public String getIdLogro() { return idLogro; }
+    public String getNombre() { return nombre; }
+    public String getDescripcion() { return descripcion; }
+    public int getBonoCreditos() { return bonoCreditos; }
+    public String getIcono() { return icono; }
+    public boolean isDesbloqueado() { return desbloqueado; }
+    public Long getFechaDesbloqueo() { return fechaDesbloqueo; }
+}
