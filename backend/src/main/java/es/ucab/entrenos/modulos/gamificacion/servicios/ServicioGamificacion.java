0ce5ed4e75067e856b3e1344991a6740393a2550
Miguel Veliz <miguelveliz152@gmail.com>
2026-06-15 23:17:58 -0400

    Conversion de TipoCriterioLogo a Enum

--- backend/src/main/java/es/ucab/entrenos/modulos/gamificacion/servicios/ServicioGamificacion.java
+++ backend/src/main/java/es/ucab/entrenos/modulos/gamificacion/servicios/ServicioGamificacion.java
@@ -62,13 +62,13 @@ public class ServicioGamificacion {
         return nuevos;
     }
 
-    private boolean cumpleCriterio(String idUsuario, String tipoCriterio) {
+    private boolean cumpleCriterio(String idUsuario, TipoCriterioLogro tipoCriterio) {
         switch (tipoCriterio) {
-            case TipoCriterioLogro.PRIMERA_TRANSACCION:
+            case PRIMERA_TRANSACCION:
                 return cumplePrimeraTransaccion(idUsuario);
-            case TipoCriterioLogro.MAESTRO_CONFIANZA:
+            case MAESTRO_CONFIANZA:
                 return cumpleMaestroConfianza(idUsuario);
-            case TipoCriterioLogro.POLIMATA:
+            case POLIMATA:
                 return cumplePolimata(idUsuario);
             default:
                 return false;
